b6e7a5771699cadcf450d5afe17fdaa0b34adb3e
shokhtalat <shokhrukhtalatov@gmail.com>
2025-10-20 01:12:41 +0900

    feat: NavBar component implemented

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,11 @@
 import Hero from "./sections/Hero.jsx";
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
+import NavBar from "./components/NavBar.jsx";
 
 const App = () => {
     return (
         <>
-
+            <NavBar />
             <Hero />
             <ShowcaseSection />
 
